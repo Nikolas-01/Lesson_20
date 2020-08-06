a02458588685910512ea5c856f217137659db146
Nikolas-01 <n32138917010m>
2020-08-06 15:30:15 +0500

    060820

--- manage.py
+++ manage.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medstat.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'automation_app.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
